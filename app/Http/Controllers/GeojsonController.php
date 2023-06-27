9078d4a355feef61d144b97200709b12cfdf7a1c
bagusindrayana <bagusindrayanaindo@gmail.com>
2023-06-27 09:55:47 +0800

    rename folder indonesia-postal-and-area

--- app/Http/Controllers/GeojsonController.php
+++ app/Http/Controllers/GeojsonController.php
@@ -14,7 +14,7 @@ class GeojsonController extends Controller
         $kabkotas = KabKota::all();
 
         foreach ($kabkotas as $kabkota) {
-            $getData = file_get_contents(public_path("Indonesia-Postal-And-Area/data/geojson/62/64/" . $kabkota->code . "/" . $kabkota->code . ".json"));
+            $getData = file_get_contents(public_path("indonesia-postal-and-area/data/geojson/62/64/" . $kabkota->code . "/" . $kabkota->code . ".json"));
             $json = json_decode($getData);
             $feature = $json->features[0];
             //jumlah kantor di kecamatan
@@ -41,7 +41,7 @@ class GeojsonController extends Controller
         $kecamatans = Kecamatan::all();
 
         foreach ($kecamatans as $kecamatan) {
-            $getData = file_get_contents(public_path("Indonesia-Postal-And-Area/data/geojson/62/64/" . $kecamatan->kabkota->code . "/" . $kecamatan->code . "/" . $kecamatan->code . ".json"));
+            $getData = file_get_contents(public_path("indonesia-postal-and-area/data/geojson/62/64/" . $kecamatan->kabkota->code . "/" . $kecamatan->code . "/" . $kecamatan->code . ".json"));
             $json = json_decode($getData);
             $feature = $json->features[0];
             //jumlah kantor di kecamatan
@@ -67,7 +67,7 @@ class GeojsonController extends Controller
         })->get();
 
         foreach ($kecamatans as $kecamatan) {
-            $getData = file_get_contents(public_path("Indonesia-Postal-And-Area/data/geojson/62/64/" . $kecamatan->kabkota->code . "/" . $kecamatan->code . "/" . $kecamatan->code . ".json"));
+            $getData = file_get_contents(public_path("indonesia-postal-and-area/data/geojson/62/64/" . $kecamatan->kabkota->code . "/" . $kecamatan->code . "/" . $kecamatan->code . ".json"));
             $json = json_decode($getData);
             // $features[] = $json->features[0];
             $feature = $json->features[0];
@@ -93,7 +93,7 @@ class GeojsonController extends Controller
             $w->where('code', $kode_kab_kota);
         })->where('code',$kode_kecamatan)->first();
 
-        $getData = file_get_contents(public_path("Indonesia-Postal-And-Area/data/geojson/62/64/" . $kecamatan->kabkota->code . "/" . $kecamatan->code . "/" . $kecamatan->code . ".json"));
+        $getData = file_get_contents(public_path("indonesia-postal-and-area/data/geojson/62/64/" . $kecamatan->kabkota->code . "/" . $kecamatan->code . "/" . $kecamatan->code . ".json"));
         $json = json_decode($getData);
         $features[] = $json->features[0];
         $format = [
